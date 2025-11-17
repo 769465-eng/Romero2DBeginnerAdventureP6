89dc1770548d14fa94c142c46d66befaecb19316
769465-eng <769465@student.dusd.net>
2025-11-17 13:52:24 -0800

    Finished  input action
    Making my chararcter move and making my game better.

--- P6RomeroAdrian2DBeginnerAdventure/Assets/Scripts/PlayerControler.cs
+++ P6RomeroAdrian2DBeginnerAdventure/Assets/Scripts/PlayerControler.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControler : MonoBehaviour
 {
+    public InputAction LeftAction;
+    public InputAction MoveAction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MoveAction.Enable();  
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.001f;
+
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+
+ Vector2 position = transform.position;
+        
+       
         transform.position = position;
 
     }
